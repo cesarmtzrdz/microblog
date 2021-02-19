b7f9ca928ef1ccc67e07b11efaa97ac115e308a8
cesar.martinez <cesar.martinez@indexzero.io>
2021-02-19 16:27:25 -0600

    sdas

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,16 +9,16 @@ class TestController extends Controller
 {
     function index(Request $request) {
         
+        info("---------- INDEX------------");
+
         if ($request->hasHeader('X-Uber-Signature')) {
             $value = $request->header('X-Uber-Signature');
-            echo "this is the header: " . $value;
+            info("this is the header: " . $value);
         }
 
         $bodyContent = $request->getContent();
-        echo "this is the body content: " . $bodyContent;
+        info("this is the body content: " . $bodyContent);
         
-        echo "yesssssss";
-
-        return redirect()->route('test')->with('msj', "Msg: {$bodyContent}");
+        info("yesssssss");
     }
 }
